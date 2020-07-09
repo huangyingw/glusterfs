e057f310d82a72a41a90bf8074e8dbe559dbc35b
Ying Huang <huangyingw@gmail.com>
2020-07-09 00:58:58 -0400

    n

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+apt-get install -y make automake autoconf libtool flex bison  \
+    pkg-config libssl-dev libxml2-dev python-dev libaio-dev       \
+    libibverbs-dev librdmacm-dev libreadline-dev liblvm2-dev      \
+    libglib2.0-dev liburcu-dev libcmocka-dev libsqlite3-dev       \
+    libacl1-dev
+
+./autogen.sh
+./configure \
+    --without-libtirpc \
+    --enable-debug \
+    --enable-gnfs \
+    --enable-asan
+
+make
+make install
